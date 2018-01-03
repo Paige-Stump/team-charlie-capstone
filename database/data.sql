89df21d59d936c17dbac7a3fc0324e781d076ff6
Paige Stump <palexisstump@gmail.com>
2018-01-03 10:06:14 -0500

    added images for landmarks

--- database/data.sql
+++ database/data.sql
@@ -7,8 +7,5 @@ BEGIN;
 -- INSERT statements go here
 
 INSERT INTO app_user (user_name, password, salt, first_name, last_name) VALUES ('cindy', 'WzYbaCzcPr6PSX0W50gUWw==', 'SDJydqT014UpMj7hCx+1/aeV7HJ82PA+CxZm0BeelT75HfxhbnAH/w5AVIHoX+A6eGiyzIMPflQlLiYhQxtB8LqddoMPWIW6jsX3MrjuKZKlRdd52az4DXdV3jWzFb+CwkGjVlLZ0SE/NvNmMxoEcwKPEEnGevnkZ8T0ZJJdh/s=', 'Cindy', 'Edington');
-INSERT INTO app_user (user_name, password, salt, first_name, last_name, admin) VALUES ('paige', 'AAAAAaaaaa;', 'Paige', 'Stump', 'true');
-INSERT INTO app_user (user_name, password, salt, first_name, last_name) VALUES ('jon', 'AAAAAaaaaa;', 'Jon', 'Wolper');
-INSERT INTO app_user (user_name, password, salt, first_name, last_name) VALUES ('chris', 'AAAAAaaaaa;', 'Chris', 'Rupp');
 
 COMMIT;
