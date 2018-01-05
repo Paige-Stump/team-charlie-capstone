07c68aa76de2920cfc1540f4b0c650ca3f1b9834
Paige Stump <palexisstump@gmail.com>
2018-01-05 16:03:31 -0500

    updates to itinerary

--- src/main/java/com/techelevator/citymap/model/ItineraryDAO.java
+++ src/main/java/com/techelevator/citymap/model/ItineraryDAO.java
@@ -4,5 +4,6 @@ import java.util.List;
 
 public interface ItineraryDAO {
 
+	public List<Itinerary> getItineraryById(String userName, String itineraryName);
 	public List<Itinerary> getAllItineraries(String userName);
 }
